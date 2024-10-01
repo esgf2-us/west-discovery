98c39de9a77dd805780b2438abeceedceb70062a
Steve Turoscy <sturoscy@globus.org>
2024-10-01 08:25:36 -0400

    Switching search index

--- src/stac_fastapi/globus_search/config.py
+++ src/stac_fastapi/globus_search/config.py
@@ -1,15 +1,13 @@
 import globus_sdk
 from stac_fastapi.types.config import ApiSettings
 
-# Initial search index from the search team
-# SEARCH_INDEX_ID = "0b717a04-73e4-47f8-a94d-1eddd8a07d49"
 
 # Index created by ALCF and populated with STAC items from netcdf files
-SEARCH_INDEX_ID = "d7814ff7-51a9-4155-8b84-97e84600acd7"
+# SEARCH_INDEX_ID = "d7814ff7-51a9-4155-8b84-97e84600acd7"
+SEARCH_INDEX_ID = "f037bb33-3413-448b-8486-8400bee5181a"
 
 
 class GlobusSearchSettings(ApiSettings):
     @property
     def create_client(self) -> globus_sdk.SearchClient:
-        # return globus_sdk.SearchClient(environment="integration")
         return globus_sdk.SearchClient()
