2996551376639970befcb303157b71ee16748c33
Steve Turoscy <sturoscy@globus.org>
2024-11-22 09:08:55 -0500

    Minor updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 # You'll need a profile (esgf2) managed by vault for account 730335463484
 aws ecr get-login-password --profile esgf2 --region us-east-1 | docker login --username AWS --password-stdin 730335463484.dkr.ecr.us-east-1.amazonaws.com
 
-docker build --platform=linux/x86_64 -f ../Dockerfile -t esgf-west-discovery:latest .
+docker build --platform=linux/x86_64 -f ../Dockerfile -t esgf-west-discovery:latest ..
 
 docker tag esgf-west-discovery:latest 730335463484.dkr.ecr.us-east-1.amazonaws.com/esgf-west-discovery:latest
 
--- src/stac_fastapi/globus_search/config.py
+++ src/stac_fastapi/globus_search/config.py
@@ -3,7 +3,6 @@ from stac_fastapi.types.config import ApiSettings
 
 
 # Index created by ALCF and populated with STAC items from netcdf files
-# SEARCH_INDEX_ID = "d7814ff7-51a9-4155-8b84-97e84600acd7"
 SEARCH_INDEX_ID = "f037bb33-3413-448b-8486-8400bee5181a"
 
 
