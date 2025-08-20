afa0610e2c1667c0e496718acef50f371b9d4584
Steve Turoscy <sturoscy@globus.org>
2025-08-20 13:44:29 -0400

    Removed 'name' key from assets list

--- src/stac_fastapi/globus_search/convert.py
+++ src/stac_fastapi/globus_search/convert.py
@@ -6,7 +6,11 @@ def search_doc_to_stac_item(search_doc):
     dict_assets = {}
     list_assets = content["assets"]
     for asset in list_assets:
-        dict_assets[asset["name"]] = asset
+        for key in list(asset):
+            if key == "name":
+                name = asset[key]
+                asset.pop(key)
+        dict_assets[name] = asset
     content["assets"] = dict_assets
 
     return content
