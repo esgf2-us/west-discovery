9186e882a075d5db2128913ffd8ac183d9dc7682
Steve Turoscy <sturoscy@globus.org>
2025-09-17 10:57:05 -0400

    Updated to 3.0.0 and through to 6.0.0

--- scripts/build/build-api.sh
+++ scripts/build/build-api.sh
@@ -7,9 +7,9 @@ docker build --platform=linux/x86_64 -f ./Dockerfile -t $API_IMAGE .
 # Uncomment below to push image to ECR in AWS
 # You'll need a profile (esgf2) managed by vault for account 730335463484
 
-aws ecr get-login-password \
-    --profile esgf2 \
-    --region us-east-1 | docker login --username AWS --password-stdin 730335463484.dkr.ecr.us-east-1.amazonaws.com
+# aws ecr get-login-password \
+#     --profile esgf2 \
+#     --region us-east-1 | docker login --username AWS --password-stdin 730335463484.dkr.ecr.us-east-1.amazonaws.com
 
-docker tag $API_IMAGE 730335463484.dkr.ecr.us-east-1.amazonaws.com/$API_IMAGE
-docker push 730335463484.dkr.ecr.us-east-1.amazonaws.com/$API_IMAGE
+# docker tag $API_IMAGE 730335463484.dkr.ecr.us-east-1.amazonaws.com/$API_IMAGE
+# docker push 730335463484.dkr.ecr.us-east-1.amazonaws.com/$API_IMAGE
