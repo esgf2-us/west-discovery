56c353021e0672e57a033ffdf1f80e9fccda0f9d
Steve Turoscy <sturoscy@globus.org>
2026-03-24 10:08:05 -0400

    Adding CMIP7 to main. Switching to integration index.

--- src/stac_fastapi/globus_search/config.py
+++ src/stac_fastapi/globus_search/config.py
@@ -1,8 +1,8 @@
 import globus_sdk
 from stac_fastapi.types.config import ApiSettings
 
-SEARCH_INDEX_ID = "c01e0cd7-a479-43a0-9264-e484fa0f64c4"  # DC6 index
-# SEARCH_INDEX_ID = "97c7e4bf-fd4d-4d4a-9eda-46141b80384f"  # integration index
+# SEARCH_INDEX_ID = "c01e0cd7-a479-43a0-9264-e484fa0f64c4"  # DC6 index
+SEARCH_INDEX_ID = "97c7e4bf-fd4d-4d4a-9eda-46141b80384f"  # integration index
 
 
 class GlobusSearchSettings(ApiSettings):
