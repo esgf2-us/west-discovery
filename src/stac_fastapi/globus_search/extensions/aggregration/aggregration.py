28ef3d72d077ea4fd195e5a41c5b447ddcf824f5
Steve Turoscy <sturoscy@globus.org>
2026-08-31 16:55:21 -0400

    Black formatting is tough

--- src/stac_fastapi/globus_search/extensions/aggregration/aggregration.py
+++ src/stac_fastapi/globus_search/extensions/aggregration/aggregration.py
@@ -5,9 +5,13 @@ from typing import Optional
 import attr
 from fastapi import Path
 from stac_fastapi.extensions.core.aggregation.request import (
-    AggregationExtensionGetRequest, AggregationExtensionPostRequest)
+    AggregationExtensionGetRequest,
+    AggregationExtensionPostRequest,
+)
 from stac_fastapi.extensions.core.filter.request import (
-    FilterExtensionGetRequest, FilterExtensionPostRequest)
+    FilterExtensionGetRequest,
+    FilterExtensionPostRequest,
+)
 from typing_extensions import Annotated
 
 
--- tests/unit/test_database_logic.py
+++ tests/unit/test_database_logic.py
@@ -7,9 +7,11 @@ import requests
 import requests.structures
 
 from stac_fastapi.globus_search import database_logic
-from stac_fastapi.globus_search.database_logic import (DatabaseLogic,
-                                                       cql_like_to_globus_like,
-                                                       cql_to_filter)
+from stac_fastapi.globus_search.database_logic import (
+    DatabaseLogic,
+    cql_like_to_globus_like,
+    cql_to_filter,
+)
 
 
 def make_search_api_error(status_code):
